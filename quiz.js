ceb741ccd3de580a70343215366d14a415a1d8f6
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-24 10:00:43 -0500

    end of quiz

--- quiz.js
+++ quiz.js
@@ -1,1 +1,45 @@
 console.log('quiz 1');
+
+function begin() {
+  console.log("begin");
+}
+begin();
+
+// 
+
+function foo() {
+  console.log("does it work?")
+}
+foo();
+
+function echo(string) {
+  return string;
+}
+echo("hello");
+//
+
+function math(x, y, z) {
+  return (x + y) * z;
+}
+math(3, 4, 2);
+
+//
+function callMe(function() {
+  console.log("in function two");
+}) {
+
+  console.log("in function one");
+};
+
+callMe();
+
+//
+
+function callMe2(string, function(string) {
+  console.log("in function two");
+  console.log(string);
+}) {
+  console.log("in function one");
+};
+
+callMe2();
